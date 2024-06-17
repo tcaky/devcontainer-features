80b7d3c15e4b70f95564f730d333f299f440590a
Keith <youngka@gmail.com>
2024-06-17 15:28:50 -0400

    finalizing feature

--- src/alpine-powershell/install.sh
+++ src/alpine-powershell/install.sh
@@ -6,4 +6,9 @@ echo "Activating feature 'alpine-powershell'"
 
 apk --no-cache add powershell
 
+if [[$VSCODESHELLSWAP == "true"]]
+    echo "Changing vscode shell to pwsh."
+    chsh vscode -s "$(which pwsh)"
+fi
+
 echo 'Done!'
