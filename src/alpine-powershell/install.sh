bc185fec338df844eeb32eba366c1f8dd952c493
Keith <youngka@gmail.com>
2024-06-18 11:39:13 -0400

    removed shell swap option - it makes no sense here.

--- src/alpine-powershell/install.sh
+++ src/alpine-powershell/install.sh
@@ -6,9 +6,4 @@ echo "Activating feature 'alpine-powershell'"
 
 apk --no-cache add powershell
 
-if [[$VSCODESHELLSWAP == "true"]]
-    echo "Changing vscode shell to pwsh."
-    chsh vscode -s "$(which pwsh)"
-fi
-
 echo 'Done!'
